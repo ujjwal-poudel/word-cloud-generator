eae800bc1581252af49453805c7a5c501b26f667
UjjwalPoudel2003 <upoudel1@my.centennialcollege.ca>
2023-07-29 04:05:31 -0400

    method to extract information from the webpage

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,8 @@ function checkInformation() {
     // Checking if the information is a link or other random text
     if (information.value.startsWith("http://") || information.value.startsWith("https://")) {
         console.log("It is a link");
+        console.log(information.value);
+        extractText(information.value);
     }
 
     else {
@@ -24,3 +26,33 @@ let submit = document.getElementById("submit-generate");
 submit.addEventListener("click", function () {
     checkInformation();
 });
+
+// Creating a function to extract the text content from website
+async function extractText(webURL) {
+  try {
+    // Fetching the webpage content
+    let response = await fetch(webURL);
+
+    // Checking if the response is successful or not
+    if (!response.ok) {
+      throw new Error("HTTP error! status: " + response.status);
+    }
+
+    // Getting the text content from the webpage
+    let content = await response.text();
+
+    // Creating a new element to store the text content
+    let element = document.createElement("div");
+    element.innerHTML = content;
+
+    // Extracting the text content from the webpage
+    let text = element.textContent;
+
+    // Checking in console
+    console.log(text);
+  }
+
+  catch (e) {
+    console.log("Error: " + e);
+  };
+};
